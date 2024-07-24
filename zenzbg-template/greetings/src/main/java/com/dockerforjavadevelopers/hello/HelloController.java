85537f8638de7c4362e38598d8d19e60abdb4fcb
jagruti4699 <80021688+jagruti4699@users.noreply.github.com>
2024-07-24 13:15:40 +0530

    Add files via upload

---
+++ zenzbg-template/greetings/src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.dockerforjavadevelopers.hello;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+		System.out.println("This is simple Spring Boot project");
+        return "Hello World. Docker deployments to AKS. .some changes...\n";
+    }
+    
+}
